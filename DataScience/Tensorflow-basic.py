a544d40007d6f6d6557310dadf25dc18f28485fa
Jihye Lee <psycho5cafe@gmail.com>
2018-06-11 19:03:39 +0900

    Create Tensorflow-basic.py

---
+++ DataScience/Tensorflow-basic.py
@@ -0,0 +1,15 @@
+#Ref: http://solarisailab.com/archives/2351
+
+import tensorflow as tf
+
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0)
+print(node1, node2)
+
+sess = tf.Session()
+print(sess.run([node1, node2]))
+
+node3= tf.add(node1, node2)
+print("node3:", node3)
+print("sess.run(node3):", sess.run(node3))
+
