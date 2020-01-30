ab017ea47b57fc9b1910c3b6b8c4f40dc9d7de7f
Jihye Lee <psycho5cafe@gmail.com>
2020-01-30 17:45:41 +0900

    Create R_setting.r

---
+++ Personal_setting/R_setting.r
@@ -0,0 +1,24 @@
+
+getwd()
+setwd(””)
+
+install.packages("ggplot")
+install.packages("dplyr")
+install.packages("arules")
+install.packages("arulesViz")
+install.packages("RGA")
+install.packages("tm")    # text mining 
+install.packages("SnowballC")
+install.packages("stringr")
+install.packages("tm")    # text mining 
+install.packages("SnowballC")
+install.packages("stringr")
+# install.packages(rJava) #자바 설치하면 "JAVA_HOME cannot be determined from the Registry" 해결됨 
+install.packages("tidyverse")
+install.packages("RColorBrewer")
+install.packages("wordcloud")
+install.packages("stringr")
+install.packages("wordcloud2")
+install.packages("data.table")
+
+installed.packages()
